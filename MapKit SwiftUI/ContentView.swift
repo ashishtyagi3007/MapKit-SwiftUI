f0bcc2d6941dc64c2f19f179ebe51a85aeeedced
Ashish Tyagi <ashisht@dewsolutions.in>
2020-11-03 11:10:38 +0530

    Initial Commit

---
+++ MapKit SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapKit SwiftUI
+//
+//  Created by Ashish Tyagi on 03/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapKit SwiftUI/MapKit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_SwiftUIApp.swift
+//  MapKit SwiftUI
+//
+//  Created by Ashish Tyagi on 03/11/20.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
